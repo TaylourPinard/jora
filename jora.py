3dd7a3b818b9a925e7c8d8a2213b3994b82a1d90
taylor <jayluvsdivn@gmail.com>
2025-10-30 16:34:12 -0700

    Implemented move_task() function to move tickets between stages of completion

--- jora.py
+++ jora.py
@@ -2,7 +2,12 @@ import csv
 import sys
 import argparse
 import os
-
+    
+    #TODO
+    # Get the parameters from the user calling the python script
+    # Delete tasks if the user would like
+    # Implement showing the user contents of their ticket system with configurable flag
+    #based command line info
 
 def main():
     count = 0
@@ -27,11 +32,6 @@ def main():
 
         create_task(title, priority, description, count)
 
-    # Get the parameters from the user calling the python script
-    # Create tasks and store them to csv in the appropriate status csv file
-    # Move tasks between status states OPEN, IN_PROGRESS, and CLOSED
-    # Delete tasks if the user would like
-
 
 def create_task(title, priority, description, count, status="OPEN"):
     file_path = f"JORA/{status}.csv"
@@ -46,6 +46,30 @@ def create_task(title, priority, description, count, status="OPEN"):
         writer.writerow([title, priority, description, count])
 
     print(f"Added new task #{count} to {status}")
+
+
+def move_task(task_id, source, destination):
+    data = []
+    
+    with open(f"JORA/{source}.csv", "r", newline="") as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        for entry in reader:
+            if entry[3] != str(task_id):
+                data.append(entry)
+            else:
+                ticket = entry
+   
+        try:
+            create_task(ticket[0], ticket[1], ticket[2], ticket[3], f"{destination}")
+        except:
+            print(f"ticket not found in {source}.csv")
+            return    
+
+    with open(f"JORA/{source}.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(data)
 
 def setup():
     os.mkdir("JORA")
